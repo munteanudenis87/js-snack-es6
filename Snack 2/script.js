5e9aba9cf48c10e4d5a6a2325df3d63a3430fc57
munteanudenis87 <munteanudenis87@gmail.com>
2026-01-11 22:46:04 +0100

    creata un nuovo array i cui elementi contengono solo nomi e falli subiti

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -3,3 +3,25 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+const squadre = [
+    {
+    nome : "Verdi",
+    puntiFatti : 0,
+    falliSubiti : 0,
+    },
+    {
+    nome : "Gialli",
+    puntiFatti : 0,
+    falliSubiti : 0,
+    },
+    {
+    nome : "Rossi",
+    puntiFatti : 0,
+    falliSubiti : 0,
+    },
+];
+const squadreFs = squadre.map(user => ({
+  nomeSquadra: user.nome,
+  falliSubitiFs: user.falliSubiti
+}));
+console.log(squadreFs);
